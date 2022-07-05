b4c978a4be06fd801a27ca379b3dfd043f228248
imamhossain1 <78858087+imamhossain1@users.noreply.github.com>
2022-07-05 16:04:52 +0600

    admin theme

---
+++ project.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 05, 2022 at 11:43 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.0.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_users`
+--
+
+CREATE TABLE `tbl_users` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `last_name` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `username` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `usertype` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_users`
+--
+
+INSERT INTO `tbl_users` (`id`, `first_name`, `last_name`, `username`, `email`, `password`, `usertype`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'iman', 'hossain', 'iman', 'iman@gmail.com', '$2y$10$93kOpFFvlxQ0q04JrMBNeebs4wOVQ6US0IEIwh44foLQHcvADeK/S', 1, 1, NULL, NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_users`
+--
+ALTER TABLE `tbl_users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_users`
+--
+ALTER TABLE `tbl_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\Theme\DashboardController;
+
+Route::get('/cache-clear',function(){
+    Artisan::call('config:cache');
+    Artisan::call('cache:clear');
+    return 'Cache is cleared';
+});
+
+Route::get('/pass',[LoginController::class,'pass'])->name('pass');
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix' => 'admin'],function(){
+      //----- Admin Auth -----
+      Route::get('/login',[LoginController::class,'adminLoginIndex'])->name('admin.login.form');
+      Route::post('/login',[LoginController::class,'adminLogin'])->name('adminLogin');
+     
+   Route::group([
+      'middleware' => ['admin_auth']
+  ],function(){
+   Route::get('dashboard',[DashboardController::class,'dashboard']);
+   // Route::get('/logout',[LoginController::class,'adminLogout']);
+
+});
+});
+
+
+Route::prefix('theme')->group(function(){
+    Route::get('dashboard', [DashboardController::class, 'dashboard'])->name('theme.dashboard');
+});
